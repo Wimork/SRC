fcfe96d8f5d0ce360dacd21a8321204f6316d1cf
Wimork <166303908+Wimork@users.noreply.github.com>
2025-02-06 13:11:01 +0100

    New start

--- sketch.js
+++ sketch.js
@@ -9,6 +9,9 @@ p22 = {x: Math.random()*1000,y: Math.random()*1000};
 p23 = {x: Math.random()*1000,y: Math.random()*1000},
 P1 = {x: 0,y: 0};
 P2 = {x: 0,y: 0};
+x = {min: 0, max: 0};
+y = {min: 0, max: 0};
+
 
 function setup() {
   createCanvas(1000, 1000);
@@ -20,6 +23,7 @@ function draw() {
     P1.y = (1 - t)*((1 - t)*((1 - t)*p10.y + p11.y*t) + ((1 - t)*p11.y + p12.y*t)*t) + ((1 - t)*((1 - t)*p11.y + p12.y*t) + ((1 - t)*p12.y + p13.y*t)*t)*t;
     P2.x = (1 - t)*((1 - t)*((1 - t)*p20.x + p21.x*t) + ((1 - t)*p21.x + p22.x*t)*t) + ((1 - t)*((1 - t)*p21.x + p22.x*t) + ((1 - t)*p22.x + p23.x*t)*t)*t;
     P2.y = (1 - t)*((1 - t)*((1 - t)*p20.y + p21.y*t) + ((1 - t)*p21.y + p22.y*t)*t) + ((1 - t)*((1 - t)*p21.y + p22.y*t) + ((1 - t)*p22.y + p23.y*t)*t)*t;
+    
     fill(0,255,0);
     noStroke()
     circle(P1.x, P1.y, 5);
@@ -28,6 +32,3 @@ function draw() {
   }
 
 }
-
-console.log(p10,p11,p12,p13);
-console.log(p20,p21,p22,p23);
