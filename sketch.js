4a99dc6749be57963af6bd9b3968c4be9c710553
Wimork <janusz999okxd@gmail.com>
2025-02-21 00:42:57 +0100

    færdig produkt

--- sketch.js
+++ sketch.js
@@ -1,62 +1,125 @@
+let p = [
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000},
+  {x: Math.random() * 1000, y: Math.random() * 1000}
+];
 
-p10 = {x: Math.random()*1000,y: Math.random()*1000};
-p11 = {x: Math.random()*1000,y: Math.random()*1000};
-p12 = {x: Math.random()*1000,y: Math.random()*1000};
-p13 = {x: Math.random()*1000,y: Math.random()*1000},
-p20 = {x: Math.random()*1000,y: Math.random()*1000};
-p21 = {x: Math.random()*1000,y: Math.random()*1000};
-p22 = {x: Math.random()*1000,y: Math.random()*1000};
-p23 = {x: Math.random()*1000,y: Math.random()*1000},
-P1 = {x: 0,y: 0};
-P2 = {x: 0,y: 0};
-x1 = {min: 1000,max: 0};
-y1 = {min: 1000,max: 0};
-x2 = {min: 1000,max: 0};
-y2 = {min: 1000,max: 0};
+let pC = [
+  {x: undefined, y: undefined},
+  {x: undefined, y: undefined}
+];
+
+let BB = [
+  {xmin: 1000, xmax: 0, ymin: 1000, ymax: 0},
+  {xmin: 1000, xmax: 0, ymin: 1000, ymax: 0}
+];
+
+let draggingPoint = null;
 
 function setup() {
   createCanvas(1000, 1000);
+  background(255);
 }
 
 function draw() {
-  for(t=0; t<1; t=t+0.001) {
-    P1.x = (1 - t)*((1 - t)*((1 - t)*p10.x + p11.x*t) + ((1 - t)*p11.x + p12.x*t)*t) + ((1 - t)*((1 - t)*p11.x + p12.x*t) + ((1 - t)*p12.x + p13.x*t)*t)*t;
-    P1.y = (1 - t)*((1 - t)*((1 - t)*p10.y + p11.y*t) + ((1 - t)*p11.y + p12.y*t)*t) + ((1 - t)*((1 - t)*p11.y + p12.y*t) + ((1 - t)*p12.y + p13.y*t)*t)*t;
-    P2.x = (1 - t)*((1 - t)*((1 - t)*p20.x + p21.x*t) + ((1 - t)*p21.x + p22.x*t)*t) + ((1 - t)*((1 - t)*p21.x + p22.x*t) + ((1 - t)*p22.x + p23.x*t)*t)*t;
-    P2.y = (1 - t)*((1 - t)*((1 - t)*p20.y + p21.y*t) + ((1 - t)*p21.y + p22.y*t)*t) + ((1 - t)*((1 - t)*p21.y + p22.y*t) + ((1 - t)*p22.y + p23.y*t)*t)*t;
-    if (P1.x > x1.max){
-      x1.max = P1.x;
-      }
-    if (P1.x < x1.min){
-      x1.min = P1.x;
-    }
-    if (P1.y > y1.max) {
-      y1.max = P1.y;
-    }
-    if (P1.y < y1.min) {
-      y1.min = P1.y;
-    }
-    if (P2.x > x2.max){
-      x2.max = P2.x;
-      }
-    if (P2.x < x2.min){
-      x2.min = P2.x;
-    }
-    if (P2.y > y2.max) {
-      y2.max = P2.y;
-    }
-    if (P2.y < y2.min) {
-      y2.min = P2.y;
-    }
-    
-    fill(0,255,0);
-    noStroke()
+  background(255);
+  
+  let newBB = [
+    { xmin: 1000, xmax: 0, ymin: 1000, ymax: 0 },
+    { xmin: 1000, xmax: 0, ymin: 1000, ymax: 0 }
+  ];
+
+  for (let t = 0; t < 1; t += 0.001) {
+    let P1 = computeBezier(p.slice(0, 4), t);
+    let P2 = computeBezier(p.slice(4, 8), t);
+    pC = [P1, P2];
+
+    newBB[0].xmin = min(P1.x, newBB[0].xmin);
+    newBB[0].xmax = max(P1.x, newBB[0].xmax);
+    newBB[0].ymin = min(P1.y, newBB[0].ymin);
+    newBB[0].ymax = max(P1.y, newBB[0].ymax);
+
+    newBB[1].xmin = min(P2.x, newBB[1].xmin);
+    newBB[1].xmax = max(P2.x, newBB[1].xmax);
+    newBB[1].ymin = min(P2.y, newBB[1].ymin);
+    newBB[1].ymax = max(P2.y, newBB[1].ymax);
+
+    fill(0, 255, 0);
+    noStroke();
     circle(P1.x, P1.y, 5);
-    fill(255,0,0)
+    fill(255, 0, 0);
     circle(P2.x, P2.y, 5);
   }
-  noFill()
-  stroke(1)
-  quad(x1.max,y1.max,x1.min,y1.max,x1.min,y1.min,x1.max,y1.min);
-  quad(x2.max,y2.max,x2.min,y2.max,x2.min,y2.min,x2.max,y2.min);
+
+  BB = newBB;
+  drawControlPoints();
+  drawBoundingBox(BB);
+  drawIntersectionBox(BB);
+}
+
+function computeBezier(points, t) {
+  let x = (1 - t) * ((1 - t) * ((1 - t) * points[0].x + points[1].x * t) + ((1 - t) * points[1].x + points[2].x * t) * t) + 
+          ((1 - t) * ((1 - t) * points[1].x + points[2].x * t) + ((1 - t) * points[2].x + points[3].x * t) * t) * t;
+  let y = (1 - t) * ((1 - t) * ((1 - t) * points[0].y + points[1].y * t) + ((1 - t) * points[1].y + points[2].y * t) * t) + 
+          ((1 - t) * ((1 - t) * points[1].y + points[2].y * t) + ((1 - t) * points[2].y + points[3].y * t) * t) * t;
+  return {x, y};
+}
+
+function drawControlPoints() {
+  for (let i = 0; i < p.length; i++) {
+    fill(i < 4 ? color(0, 255, 0) : color(255, 0, 0));
+    stroke(1);
+    circle(p[i].x, p[i].y, 10);
+  }
+}
+
+function drawBoundingBox(BB) {
+  for (let i = 0; i < BB.length; i++) {
+    stroke(1);
+    noFill();
+    quad(
+      BB[i].xmin, BB[i].ymin,
+      BB[i].xmax, BB[i].ymin,
+      BB[i].xmax, BB[i].ymax,
+      BB[i].xmin, BB[i].ymax
+    );
+  }
+}
+
+function drawIntersectionBox(BB) {
+  let xOverlapMin = max(BB[0].xmin, BB[1].xmin);
+  let xOverlapMax = min(BB[0].xmax, BB[1].xmax);
+  let yOverlapMin = max(BB[0].ymin, BB[1].ymin);
+  let yOverlapMax = min(BB[0].ymax, BB[1].ymax);
+
+  if (xOverlapMin < xOverlapMax && yOverlapMin < yOverlapMax) {
+    fill(255, 0, 0, 100);
+    noStroke();
+    rect(xOverlapMin, yOverlapMin, xOverlapMax - xOverlapMin, yOverlapMax - yOverlapMin);
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < p.length; i++) {
+    if (dist(mouseX, mouseY, p[i].x, p[i].y) < 10) {
+      draggingPoint = i;
+      break;
+    }
+  }
+}
+
+function mouseDragged() {
+  if (draggingPoint !== null) {
+    p[draggingPoint].x = mouseX;
+    p[draggingPoint].y = mouseY;
+  }
+}
+
+function mouseReleased() {
+  draggingPoint = null;
 }
